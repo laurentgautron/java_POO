54ec6b0d12cef47cc2b646f2647195915401a340
laurent GAUTRON <laurentgautron@protonmail.com>
2022-07-12 14:09:57 +0200

    class Avion with constructor

--- src/POO/Avion.java
+++ src/POO/Avion.java
@@ -1,11 +1,22 @@
 package POO;
 
+// constructeur par défaut par défaut : c'est une version minimale du constructeur que la JVM va appeler
+// si aucun constructeur n'est défini
+// c'est pourquoi lorsqu'on ne définit aucune valeur pour l'avion, il initialise les attributs
+
 public class Avion {
 
     private String fabricant;
     private String modele;
     private int nombreSieges;
     private String couleur;
+
+    public Avion(String fabricant, String modele, int nombreSieges, String couleur) {
+        this.fabricant = fabricant;
+        this.modele = modele;
+        this.nombreSieges = nombreSieges;
+        this.couleur = couleur;
+    }
 
     public String getFabricant() {
         return fabricant;
--- src/POO/Main.java
+++ src/POO/Main.java
@@ -3,11 +3,11 @@ package POO;
 public class Main {
     public static void main(String[] args) {
         // Objet = instance
-        Avion boeing737Map = new Avion();
-        boeing737Map.setFabricant( "boeing");
+        Avion boeing737Map = new Avion("boeing","737 Max 10", 200, "white");
+        /*boeing737Map.setFabricant( "boeing");
         boeing737Map.setModele("737 Max 10");
         boeing737Map.setNombreSieges(200);
-        boeing737Map.setCouleur("white");
+        boeing737Map.setCouleur("white");*/
         System.out.printf("%s\n=======================\n", boeing737Map.nomCompletAvion());
         System.out.printf("Marque: %s\nModèle: %s\nNombre de siège: %d\nCouleur: %s\n",
                 boeing737Map.getFabricant(),
@@ -15,11 +15,7 @@ public class Main {
                 boeing737Map.getNombreSieges(),
                 boeing737Map.getCouleur());
 
-        Avion airbusA320 = new Avion();
-        airbusA320.setFabricant("airbus");
-        airbusA320.setModele("A320");
-        airbusA320.setNombreSieges(150);
-        airbusA320.setCouleur("white");
+        Avion airbusA320 = new Avion("airbus", "A320", 150, "white");
         System.out.printf("%s\n=======================\n", airbusA320.nomCompletAvion());
         System.out.printf("Marque: %s\nModèle: %s\nNombre de sièges: %d\nCouleur: %s\n",
                 airbusA320.getFabricant(),
@@ -27,17 +23,17 @@ public class Main {
                 airbusA320.getNombreSieges(),
                 airbusA320.getCouleur());
 
-        Avion monAvion = new Avion(); // si on ne renseigne pas les données
+        // Avion monAvion = new Avion(); // si on ne renseigne pas les données
         // toutes les variables d'origine primitive seront à 0 (pour les int)
         // à null (pour les String)
-        System.out.printf("Marque: %s\nModèle: %s\nNombre de sièges: %d\nCouleur: %s\n",
+        /*System.out.printf("Marque: %s\nModèle: %s\nNombre de sièges: %d\nCouleur: %s\n",
                 monAvion.getFabricant(),
                 monAvion.getModele(),
                 monAvion.getNombreSieges(),
-                monAvion.getCouleur());
+                monAvion.getCouleur());*/
 
         // opérations objet Rectangle
-        Rectangle rec = new Rectangle();
+        /*Rectangle rec = new Rectangle();
         rec.setLargeur(200.5);
         rec.setLongueur(150.3);
         System.out.println("##############################");
@@ -46,6 +42,6 @@ public class Main {
         System.out.println("Longueur: " + rec.getLongueur());
         System.out.println("Largeur: " + rec.getLargeur());
         System.out.println("La surface du rectangle est: " + rec.calculAire());
-        System.out.println("Le périmètre du rectangle est: " + rec.calculPerimetre());
+        System.out.println("Le périmètre du rectangle est: " + rec.calculPerimetre());*/
     }
 }

97d6840b4d6e41f01e2c5cd97187096fc63c5897
laurent GAUTRON <laurentgautron@protonmail.com>
2022-07-12 11:06:04 +0200

    add mnoAvion withoout variables

--- src/POO/Main.java
+++ src/POO/Main.java
@@ -21,11 +21,20 @@ public class Main {
         airbusA320.nombreSieges = 150;
         airbusA320.couleur = "white";
         System.out.printf("%s\n=======================\n", airbusA320.nomCompletAvion());
-        System.out.printf("Marque: %s\nModèle: %s\nNombre de sièges: %d\nCouleur: %s",
+        System.out.printf("Marque: %s\nModèle: %s\nNombre de sièges: %d\nCouleur: %s\n",
                 airbusA320.fabricant,
                 airbusA320.modele,
                 airbusA320.nombreSieges,
                 airbusA320.couleur);
+
+        Avion monAvion = new Avion(); // si on ne renseigne pas les données
+        // toutes les variables d'origine primitive seront à 0 (pour les int)
+        // à null (pour les String)
+        System.out.printf("Marque: %s\nModèle: %s\nNombre de sièges: %d\nCouleur: %s",
+                monAvion.fabricant,
+                monAvion.modele,
+                monAvion.nombreSieges,
+                monAvion.couleur);
 
     }
 }
